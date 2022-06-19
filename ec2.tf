d06656def4a7b7c9ba7debaa84778c9ca6e99416
saikumarsagar <102918746+saikumarsagar@users.noreply.github.com>
2022-06-19 18:23:51 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,8 +1,4 @@
-provider "aws" {
-  access_key = "AKIAQGDPOP3MK2BPOEGY"
-  secret_key = "RiL122ZCJ11Ixvi8NouCYmkqiPUkxPErU7RA39TN"
-  region     = "ap-south-1"
-}
+
 
 resource "aws_instance" "example" {
   ami           = "ami-04893cdb768d0f9ee"
